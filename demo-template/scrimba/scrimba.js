00b07ed4cf799b51a0bd1df658ba27315171b3ad
Diogo Silva <digas.silva@icloud.com>
2022-10-01 16:26:00 +0100

    Initialized javascript course.

---
+++ demo-template/scrimba/scrimba.js
@@ -0,0 +1,117 @@
+// 1. Create a variable, myAge, and set its value to your age
+let myAge = 35
+
+// 2. Log the myAge variable to the console
+console.log(myAge)
+
+
+
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+let myAge = 35
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
+
+
+
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+
+
+
+
+
+
+
+
+
+// on click event listener
+function increment() {
+    console.log("The button was clicked")
+}
+
+
+
+
+
+
+
+
+
+// Create a function (you decide the name) that logs out the number 42 to the console
+// Call/invoke the function
+
+function myLogger() {
+    console.log(42)
+}
+
+myLogger()
+
+
+
+
+
+
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
+
+console.log(totalTime)
+
+
+
+
+
+
+
+
+
+
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function incrementLap() {
+    lapsCompleted = lapsCompleted + 1
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
+
+console.log(lapsCompleted)
+
+
+
+
